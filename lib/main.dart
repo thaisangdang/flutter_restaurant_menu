b8732ccd1a402c4c6faead106526e76436c47e53
thaisangdang <thaisangdang@gmail.com>
2025-12-03 12:17:47 +0700

    Implement ListView.builder in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
+import 'models/food_item.dart';
+import 'components/menu_card.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RestaurantApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RestaurantApp extends StatelessWidget {
+  const RestaurantApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor:
+            Colors.grey[100], // Light grey background makes white cards pop
+        appBar: AppBar(
+          title: const Text('Menu', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.shopping_cart, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        // THE LIST BUILDER
+        body: ListView.builder(
+          // 1. How many items do we have?
+          itemCount: menu.length,
+
+          // 2. Build each item by index
+          itemBuilder: (context, index) {
+            // Get the specific food object
+            final food = menu[index];
+
+            // Return our custom card
+            return MenuCard(
+              food: food,
+              onTap: () {
+                // Show a feedback message
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('${food.name} added to cart!'),
+                    duration: const Duration(seconds: 1),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
